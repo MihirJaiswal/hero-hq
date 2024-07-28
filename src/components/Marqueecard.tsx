f6ca27a3c863b919687f91186b98f9089cba05cc
Mihir Jaiswal <jaiswalmihir.business@gmail.com>
2024-07-28 23:18:31 +0530

    some changes

--- src/components/Marqueecard.tsx
+++ src/components/Marqueecard.tsx
@@ -55,8 +55,8 @@ const ReviewCard = ({ img }: { img: string }) => {
 export function MarqueeCard() {
   return (
     <div className="relative h-[60vh] flex py-1 w-full flex-col items-center justify-center overflow-hidden md:shadow-xl bg-bg2 md:bg-bg7">
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full h-[109vh] overflow-hidden">
-        <div className="absolute inset-x-0 -bottom-96 h-full bg-[#08090D] opacity-80 rounded-t-full"></div>
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[110vw] h-[111vh] overflow-hidden">
+        <div className="absolute inset-x-0 -bottom-96 h-full bg-bg4 opacity-80 rounded-t-full"></div>
       </div>
       
       <Marquee pauseOnHover className="[--duration:20s]">
--- src/components/heros/Container.tsx
+++ src/components/heros/Container.tsx
@@ -4,7 +4,7 @@ import Superhero from './Superhero';
 const Container = () => {
   return (
     <div className='md:bg-bg2' id='explore'>
-      <section className={`md:bg-bg3 text-white md:p-8 p-3 shadow-lg md:mx-6 relative overflow-hidden transition-opacity duration-700 ease-out transform border-t border-gray-700  `}>
+      <section className={`md:bg-bg3 bg-bg2 text-white md:p-8 p-3 shadow-lg md:mx-6 relative overflow-hidden transition-opacity duration-700 ease-out transform border-t border-gray-700  `}>
         <Superhero/>
     </section>
     </div>
